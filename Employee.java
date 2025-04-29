5cb9c5ba5c5dc7f17b60a013a5b6d6ec76e78ed3
deepajadhav1107 <deepajadhav2001>
2025-04-29 08:17:42 +0530

    save

---
+++ Employee.java
@@ -0,0 +1,28 @@
+
+public class Employee {
+    // Fields (attributes)
+    String name;
+    int id;
+    double salary;
+
+    // Constructor
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Method to display employee details
+    public void displayInfo() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Creating an object of Employee
+        Employee emp1 = new Employee("Deepa", 101, 50000.0,"Belagavi");
+        emp1.displayInfo();
+    }
+}
